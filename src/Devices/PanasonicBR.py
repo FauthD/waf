8d1fbb5c8ee2afb7534081f75a42420a856cb035
fauthd <19874899+FauthD@users.noreply.github.com>
2024-02-28 17:50:06 +0100

    Bring in Panasonic blueray code.

--- src/Devices/PanasonicBR.py
+++ src/Devices/PanasonicBR.py
@@ -19,9 +19,57 @@
 #
 
 import time
+import logging
+import telnetlib
+
 from . import Device
 
 class PanasonicBR(Device):
 	def __init__(self, dev_config:dict, count, send):
 		super().__init__(dev_config, count, send)
 
+	def SendTelnet(self, cmd):
+		logging.debug(f'SendTelnet {cmd} to {self.getName()}')
+		try:
+			tn = telnetlib.Telnet(host=self._devicename, port=8102, timeout=2)
+			#tn.set_debuglevel(20)
+			tn.write(cmd.encode('ascii') + b"\r\n")
+			response = tn.read_until(b'R', 1)
+			logging.debug(f'Telnet response={response} from {self.getName()}')
+			tn.close()
+		except:
+			logging.debug(f'SendTelnet to {self.getName()} failed')
+
+
+	def TurnOn(self):
+		super().TurnOn()
+		#pexpect.run('irsend SEND_ONCE RC-2422 KEY_ENTER')
+		#pexpect.run('irsend SEND_ONCE RC-2422 KEY_POWER')
+		#self.WakeOnLan()
+		self.SendTelnet('PN')
+		self._On = self.WaitForHost()
+
+	def TurnOff(self):
+		#pexpect.run('irsend SEND_ONCE RC-2422 KEY_ENTER')
+		#pexpect.run('irsend SEND_ONCE RC-2422 KEY_POWER')
+		if self._On:
+			self.SendTelnet('PF')
+			self._On = False
+
+	def WatchBrMovie(self):
+		self.TurnOn()
+
+	def WatchTV(self):
+		self.TurnOff()
+
+	def WatchTvMovie(self):
+		self.TurnOff()
+
+	def ListenMusic(self):
+		self.TurnOff()
+
+	def ListenRadio(self):
+		self.TurnOff()
+
+	def ListenIRadio(self):
+		self.TurnOff()
